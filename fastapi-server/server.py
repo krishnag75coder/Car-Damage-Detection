ff5f13e94e98c467bc13f86768b626aaa2ea8498
Krishna Gupta <krishnavarshnsy300@gmail.com>
2026-02-01 03:49:23 +0530

    some changes

--- fastapi-server/server.py
+++ fastapi-server/server.py
@@ -1,28 +1,17 @@
-import logging
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI,File,UploadFile
 from model_helper import predict
-
 app = FastAPI()
-
-logging.basicConfig(level=logging.INFO)
-
 @app.post("/predict")
 async def get_prediction(file: UploadFile = File(...)):
     try:
-        logging.info(f"Received file: {file.filename}")
-
         image_bytes = await file.read()
+
         image_path = "temp_file.jpg"
+
         with open(image_path, "wb") as f:
             f.write(image_bytes)
 
-        logging.info(f"Saved image to {image_path}")
-
         prediction = predict(image_path)
-        logging.info(f"Prediction: {prediction}")
-
-        return {"prediction": prediction}
-
+        return {'prediction': prediction}
     except Exception as e:
-        logging.error(f"Error: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+        return {'error': str(e)}
--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -1,28 +1,13 @@
 import streamlit as st
-import requests
-
-# Access secrets
-API_URL = st.secrets["api_keys"]["fastapi_url"]
-db_user = st.secrets["database"]["user"]
-db_pass = st.secrets["database"]["password"]
+from model_helper import predict
 
 st.title("Car Damage Detection")
-
-uploaded_file = st.file_uploader("Upload an image", type=["png","jpg","jpeg"])
+uploaded_file = st.file_uploader("Upload the image",type=["png","jpg","jpeg"])
 
 if uploaded_file:
-    files = {"file": ("image.jpg", uploaded_file.getvalue(), uploaded_file.type)}
-    response = requests.post(API_URL, files=files)
-
-    if response.status_code == 200:
-        st.success(f"Prediction: {response.json()['prediction']}")
-    else:
-        # Show raw text if JSON fails
-        try:
-            st.error(response.json().get("detail", "Backend error"))
-        except:
-            st.error(f"Backend error (raw): {response.text}")
-
-
-# Optional: show database info (just for testing, normally don't display passwords)
-st.write(f"DB User: {db_user}")
+    image_path = "temp_file.jpg"
+    with open(image_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+        st.image(image_path, caption="Uploaded Image", use_container_width=True)
+        prediction = predict(image_path)
+        st.info(f"Prediction: {prediction}")
